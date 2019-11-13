8de32f773306f3dd865a997e689722a7064525fe
firerain42 <firerain42@googlemail.com>
2019-11-13 09:50:24 +0100

    Alias nvim with vim only if nvim is installed

--- zsh/zsh/lib/aliases.zsh
+++ zsh/zsh/lib/aliases.zsh
@@ -3,10 +3,15 @@ alias la='ls -lah'
 alias ...='cd ../..'
 alias -g no='2> /dev/null > /dev/null'
 alias open="xdg-open no"
-alias vim="nocorrect nvim"
 alias tree="nocorrect tree -C"
 alias g="tig status"
 
 if type stack &> /dev/null; then
     alias ghci="stack ghci --package classy-prelude --ghc-options -XNoImplicitPrelude --main-is ClassyPrelude"
+fi
+
+if type nvim &> /dev/null; then
+    alias vim="nocorrect nvim"
+else
+    alias vim="nocorrect vim"
 fi
